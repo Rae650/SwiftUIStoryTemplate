89442b07d4713d6af1c28b8b9de01592ecbd4ea0
Rae650 <pricemaritza94@yahoo.com>
2023-11-13 13:43:24 -0500

    Still working on it guys some things i gotta fix but i gotta take a little break lol

---
+++ SwiftUIStoryTemplate/bookCover.swift
@@ -0,0 +1,33 @@
+//
+//  bookCover.swift
+//  SwiftUIStoryTemplate
+//
+//  Created by Maritza Price on 11/13/23.
+//
+
+import SwiftUI
+struct bookCover: View {
+    var body: some View {
+     
+        
+            Image("BS")
+            .resizable()
+            .ignoresSafeArea()
+            .background(ignoresSafeAreaEdges: Edge .Set.bottom)
+            .imageScale(.small)
+        
+        
+          
+            
+             
+                
+            
+            
+        
+        
+    }
+    
+}
+#Preview {
+    bookCover()
+}
