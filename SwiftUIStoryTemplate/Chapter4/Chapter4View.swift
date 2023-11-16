ab81df1ee50936e08e604930d4dcb112ef9f9b20
Mya M <bornagoat1214@icloud.com>
2023-11-16 08:22:10 -0500

    dfddbfcbvfd

--- SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
+++ SwiftUIStoryTemplate/Chapter4/Chapter4View.swift
@@ -2,10 +2,30 @@ import SwiftUI
 
 struct Chapter4View: View {
     var body: some View {
+       
+    
         Text("Chapter 4")
+            .offset(x: 1,y: -130)
+            .bold()
+            .font(.largeTitle)
+        
+        
+        Image("DON")
+            .resizable()
+            .frame(width: 500, height: 400)
+            .offset(x: 10,y: -85)
+            
+        
+        
+        
+    
+        
+        
+        
     }
 }
 
 #Preview {
     Chapter4View()
 }
+        
